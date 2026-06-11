ffca835541814ad8ca9b5aa04e8b9aa54d98b6c3
Lilssen Kleber Fuzaro <lilssends@gmail.com>
2026-06-11 15:15:26 -0300

    Create app.controller.ts

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get('health')
+  getHealth() {
+        return {
+                status: 'ok',
+                timestamp: new Date().toISOString(),
+                service: 'Compra Eficiente API',
+                version: '1.0.0',
+          };
+}
+}
