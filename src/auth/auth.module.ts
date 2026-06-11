c3f8a74a81891e8acc091191d079f0bca3735cbe
Lilssen Kleber Fuzaro <lilssends@gmail.com>
2026-06-11 15:15:46 -0300

    Create auth.module.ts

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secret',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  providers: [],
+  exports: [JwtModule],
+})
+export class AuthModule {}
