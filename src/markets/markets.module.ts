b0f6898dbbae4d2d1aed94420584d8b66865c1d6
Lilssen Kleber Fuzaro <lilssends@gmail.com>
2026-06-11 20:13:21 -0300

    Add MarketsService and MarketsController to MarketsModule

--- src/markets/markets.module.ts
+++ src/markets/markets.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MarketsService } from './markets.service';
+import { MarketsController } from './markets.controller';
 
-@Module({ providers: [], exports: [] })
+@Module({
+  controllers: [MarketsController],
+  providers: [MarketsService],
+  exports: [MarketsService],
+})
 export class MarketsModule {}
