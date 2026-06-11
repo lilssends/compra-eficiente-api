65003e6f90c7f810d2eb36d823f0a9a531843196
Lilssen Kleber Fuzaro <lilssends@gmail.com>
2026-06-11 16:49:41 -0300

    Add AuthService to AuthModule providers and exports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [AuthGuard],
-  exports: [AuthGuard],
+  providers: [AuthGuard, AuthService],
+  exports: [AuthGuard, AuthService],
 })
 export class AuthModule {}
