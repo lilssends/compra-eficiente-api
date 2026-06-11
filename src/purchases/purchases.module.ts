53f96b6747eee83000114795d80dc810ccc8c6d7
Lilssen Kleber Fuzaro <lilssends@gmail.com>
2026-06-11 20:19:29 -0300

    Add controller and service to PurchasesModule

--- src/purchases/purchases.module.ts
+++ src/purchases/purchases.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PurchasesService } from './purchases.service';
+import { PurchasesController } from './purchases.controller';
 
-@Module({ providers: [], exports: [] })
+@Module({
+  controllers: [PurchasesController],
+  providers: [PurchasesService],
+  exports: [PurchasesService],
+})
 export class PurchasesModule {}
