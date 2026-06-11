e4954cd7ad860b7b0e46c10c97e786b94e6f86b4
Lilssen Kleber Fuzaro <lilssends@gmail.com>
2026-06-11 15:40:19 -0300

    feat: update UsersModule with service and controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { AuthModule } from '../auth/auth.module';
 
-@Module({ providers: [], exports: [] })
-export class UsersModule {}
+@Module({
+    imports: [AuthModule],
+    controllers: [UsersController],
+    providers: [UsersService],
+    exports: [UsersService],
+})
+  export class UsersModule {}
